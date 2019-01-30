18f2b9f34b78a482287c678fb6f2029a57e66ad4
rabreau <rabreau@unistra.fr>
2019-01-30 15:56:14 +0100

    reset default field name

--- project/boards/models.py
+++ project/boards/models.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Board(models.Model):
-    name = models.CharField(max_length=30, unique=True, name='Domaine')
+    name = models.CharField(max_length=30, unique=True, verbose_name='Domaine')
     description = models.CharField(max_length=100)
 
 
 class Topic(models.Model):
-    subject = models.CharField(max_length=255, name='Sujet')
-    last_update = models.DateTimeField(auto_now_add=True, name='Dernier ajout')
+    subject = models.CharField(max_length=255, verbose_name='Sujet')
+    last_update = models.DateTimeField(auto_now_add=True, verbose_name='Dernier ajout')
     board = models.ForeignKey(Board, related_name='topics')
     starter = models.ForeignKey(User, related_name='topics')
 
@@ -18,8 +18,8 @@ class Topic(models.Model):
 class Post(models.Model):
     message = models.TextField(max_length=4000)
     topic = models.ForeignKey(Topic, related_name='posts')
-    created_at = models.DateTimeField(auto_now_add=True, name='écrit le')
-    updated_at = models.DateTimeField(null=True, name='édité le')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='écrit le')
+    updated_at = models.DateTimeField(null=True, verbose_name='édité le')
     created_by = models.ForeignKey(User, related_name='posts')
     updated_by = models.ForeignKey(User, null=True, related_name='+')
 
