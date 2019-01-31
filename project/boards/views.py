6aba11a398b02a77107282d12d91858f4a8e632e
rabreau <rabreau@unistra.fr>
2019-01-31 15:12:18 +0100

    naïv handle new topic post

--- project/boards/views.py
+++ project/boards/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.http import Http404
-from boards.models import Board
+from boards.models import Board, Topic, Post
 # Create your views here.
 
 
@@ -22,6 +23,27 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+
     context = {'board': board}
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()  # update with current user
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user,
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user,
+        )
+
+        return redirect('board_topics', pk=board.pk)  # redirect to topic messages page
 
     return render(request, 'new_topic.html', context)
