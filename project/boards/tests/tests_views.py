d5887a88ac01a19428b42fda2cb577788ceab80a
rabreau <rabreau@unistra.fr>
2019-02-04 17:08:13 +0100

    added password reset

--- project/boards/tests/tests_views.py
+++ project/boards/tests/tests_views.py
@@ -3,9 +3,9 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.urls import resolve
 
-from .models import Board, Topic, Post
-from .forms import NewTopicForm
-from .views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
+from ..views import home, board_topics, new_topic
 
 
 # Create your tests here.
