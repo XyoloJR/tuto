94dc9369d8c1e731f8ce9dff2184902034b1a15f
rabreau <rabreau@unistra.fr>
2019-02-06 15:09:28 +0100

    added post form in reply topic view

--- project/boards/views.py
+++ project/boards/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 
-from .forms import NewTopicForm
+from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
 
 
@@ -52,4 +52,21 @@ def new_topic(request, pk):
 def reply_topic(request, pk, topic_pk):
     topic = get_object_or_404(Topic, pk=topic_pk, board__pk=pk)
 
-    return render(request, 'reply_topic.html')
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+
+            return redirect('board_topics', pk=pk)  # should redirect to topic posts
+            # return redirect('topic_post', pk=pk, topic_pk=topic_pk)
+
+    else:
+        form = PostForm()
+
+    context = {'topic': topic, 'form': form}
+
+    return render(request, 'reply_topic.html', context)
