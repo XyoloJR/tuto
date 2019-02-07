656dad9b9cb97cbb8076ee3c9bb094228e30a6bf
rabreau <rabreau@unistra.fr>
2019-02-07 16:45:35 +0100

    added Update class View for post update

--- project/boards/views.py
+++ project/boards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count
+from django.views.generic import UpdateView
+from django.utils import timezone
 
 from .forms import NewTopicForm, PostForm
 from .models import Board, Post, Topic
@@ -87,3 +89,18 @@ def reply_topic(request, pk, topic_pk):
     context = {'topic': topic, 'form': form}
 
     return render(request, 'reply_topic.html', context)
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topik_pk=post.topic.pk)
