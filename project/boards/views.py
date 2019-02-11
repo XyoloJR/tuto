aee71a0f4292db8e96654469f3c6c87d64bf8cee
rabreau <rabreau@unistra.fr>
2019-02-11 15:47:25 +0100

    customed pagination

--- project/boards/views.py
+++ project/boards/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 from django.db.models import Count
 from django.views.generic import UpdateView, ListView
 from django.utils import timezone
@@ -141,7 +142,14 @@ def reply_topic(request, pk, topic_pk):
             topic.last_update = timezone.now()
             topic.save()
 
-            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+            topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
+            topic_post_url = '{url}?page={page}#{id}'.format(
+                url=topic_url,
+                id=post.pk,
+                page=topic.get_page_count()
+            )
+
+            return redirect(topic_post_url)
 
     else:
         form = PostForm()
